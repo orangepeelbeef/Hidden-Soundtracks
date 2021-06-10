a61debc66f22ea0999f03865771f291e7ee24b64
kandashi <jacobmpalmer@live.co.uk>
2021-06-10 10:57:34 +0100

    little fixes

--- scripts/hideTracks.js
+++ scripts/hideTracks.js
@@ -1,5 +1,8 @@
 Hooks.once('ready', async function() {
     if(!game.user.isGM){
         document.documentElement.style.setProperty('--myDisplay', "none");
+        document.documentElement.style.setProperty('--myHidden', "hidden");
+        document.documentElement.style.setProperty('--myFlex', "none");
+
     }
 });
